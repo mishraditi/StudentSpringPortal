245b872c41d23055aa4882bf98753ec0063d30d1
Aditi Mishra <aditimishra91924@gmail.com>
2024-12-23 17:25:20 +0530

    Added login functionality: LoginController and login.jsp

---
+++ src/main/java/com/portal/student/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.portal.student.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("login")
+    public String gotoLoginPage() {
+        return "login";
+    }
+}
