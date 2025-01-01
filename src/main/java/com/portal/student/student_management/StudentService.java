6c90487d507cfc04bd710a04f4ed6202a3d9a1cd
Aditi Mishra <aditimishra91924@gmail.com>
2025-01-01 23:33:09 +0530

    Implement student addition in service.

--- src/main/java/com/portal/student/student_management/StudentService.java
+++ src/main/java/com/portal/student/student_management/StudentService.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+//import static com.sun.beans.introspect.PropertyInfo.Name.description;
+
 @Service
 public class StudentService {
 private static List<Student> students = new ArrayList<>();
+private static int studentCount = 0;
 
 static {
-    students.add(new Student(1, "Alice", 15, "10th Grade", "123 Main St","Mr. Smith"));
-    students.add(new Student(2, "Bob", 16, "11th Grade", "456 Oak Rd","Mr. Smith"));
-    students.add(new Student(3, "Charlie", 14, "9th Grade", "789 Pine Ave","Ms. Johnson"));
-    students.add(new Student(4, "Diana", 17, "12th Grade", "101 Maple Dr","Mr. Smith"));
+    students.add(new Student(++studentCount, "Alice", 15, "10th Grade", "123 Main St","Mr. Smith"));
+    students.add(new Student(++studentCount, "Bob", 16, "11th Grade", "456 Oak Rd","Mr. Smith"));
+    students.add(new Student(++studentCount, "Charlie", 14, "9th Grade", "789 Pine Ave","Ms. Johnson"));
+    students.add(new Student(++studentCount, "Diana", 17, "12th Grade", "101 Maple Dr","Mr. Smith"));
 }
 
 public List<Student> getAllStudents() {
@@ -28,4 +31,12 @@ public List<Student> getAllStudents() {
      = student -> student.getTeacherName().equalsIgnoreCase(name); //parameter representing a single Student object from the list.
     return students.stream().filter(predicate).toList();
     }
+
+    public void addStudent(String name, int age, String grade, String address ,String teacherName){
+        Student student = new Student(++studentCount,name,age,grade,address,teacherName);
+        students.add(student);
+//         System.out.println("Student added: " + student);
+//        System.out.prtintln("Current students list: " + students);
+    }
+
 }
