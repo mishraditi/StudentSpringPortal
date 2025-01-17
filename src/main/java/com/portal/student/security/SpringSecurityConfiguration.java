36b4e185d6e42d56c0f809d821f5135ccf5b4c24
Aditi Mishra <aditimishra91924@gmail.com>
2025-01-17 09:10:32 +0530

    Configuring Spring Security with Custom User and Password

---
+++ src/main/java/com/portal/student/security/SpringSecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.portal.student.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+    //LDAP or Database
+    //In Memory
+
+    //InMemoryUserDetailsManager
+    //InMemoryUserDetailsManager(UserDetails... users)
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        Function<String, String> passwordEncoder
+                = input -> passwordEncoder().encode(input);
+
+        UserDetails userDetails = User.builder()
+                .passwordEncoder(passwordEncoder)
+
+                .password("dummy")
+                .roles("USER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
